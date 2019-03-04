7718b90ae921feef5b05caf3e50bc08da8c3d108
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2019-03-04 18:51:11 +0600

    52/62 Blog With PHP OOP (Creating Multi-Style Themes) : Part-49 of Blog With PHP OOP Program Completed.

--- inc/header.php
+++ inc/header.php
@@ -8,69 +8,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-<?php
-	if (isset($_GET['pageid'])) {
-		$pagetitleid = $_GET['pageid'];
-		$query = "SELECT * FROM tbl_page WHERE id='$pagetitleid'";
-        $pages = $db->select($query);
-            if ($pages) {
-                while ($result = $pages->fetch_assoc()) {?>
-                <title><?php echo $result['name'];?>-<?php echo TITLE;?></title>
-            <?php } } }elseif (isset($_GET['id'])) {
-			$postid = $_GET['id'];
-			$query = "SELECT * FROM tbl_post WHERE id='$postid'";
-        	$posts = $db->select($query);
-            if ($posts) {
-                while ($result = $posts->fetch_assoc()) {?>
-                <title><?php echo $result['title'];?>-<?php echo TITLE;?></title>
-            <?php } } }
-            else{?>
-            	<title><?php echo $fm->title();?>-<?php echo TITLE;?></title>
-            <?php	}?>
+    <?php include 'scripts/meta.php'?>
+    <?php include 'scripts/css.php'?>
+	<?php include 'scripts/js.php'?>
 
-	<meta name="language" content="English">
-	<meta name="description" content="It is a website about education">
-	<?php
-		if (isset($_GET['id'])) {
-			$keywordsid = $_GET['id'];
-			$query = "SELECT * FROM tbl_post WHERE id='$keywordsid'";
-        	$keywordsid = $db->select($query);
-        	if ($keywordsid) {
-        		while ($result = $keywordsid->fetch_assoc()) {
-    ?>
-    <meta name="keywords" content="<?php echo $result['tags'];?>">
-    <?php } } }else{?>
-	<meta name="keywords" content="<?php echo KEYWORDS;?>">
-    <?php } ?>
-
-	<meta name="author" content="zaman">
-	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">	
-	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="style.css">
-	<script src="js/jquery.js" type="text/javascript"></script>
-	<script src="js/jquery.nivo.slider.js" type="text/javascript"></script>
-
-<script type="text/javascript">
-$(window).load(function() {
-	$('#slider').nivoSlider({
-		effect:'random',
-		slices:10,
-		animSpeed:500,
-		pauseTime:5000,
-		startSlide:0, //Set starting Slide (0 index)
-		directionNav:false,
-		directionNavHide:false, //Only show on hover
-		controlNav:false, //1,2,3...
-		controlNavThumbs:false, //Use thumbnails for Control Nav
-		pauseOnHover:true, //Stop animation while hovering
-		manualAdvance:false, //Force manual transitions
-		captionOpacity:0.8, //Universal caption opacity
-		beforeChange: function(){},
-		afterChange: function(){},
-		slideshowEnd: function(){} //Triggers after all slides have been shown
-	});
-});
-</script>
 </head>
 
 <body>
---
+++ scripts/css.php
@@ -0,0 +1,155 @@
+<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="style.css">
+<style>
+    /*Default*/
+    .headersection, .footersection { background: #ca932f none repeat scroll 0 0; }
+    .icon a { background: #a56e0a none repeat scroll 0 0; }
+    .searchbtn input[type="text"] { background: #FFE07C; border: 1px solid #A56E0A; }
+    .searchbtn input[type="submit"] {border: 1px solid #9d6602;}
+    input[type="submit"] {
+        background: #b7801c none repeat scroll 0 0;
+        border: 1px solid #e6af4b;
+    }
+    .searchbtn input[type="submit"]:hover {
+        background: #b7801c none repeat scroll 0 0;
+        border: 1px solid #e6af4b;
+    }
+    .navsection {
+        background: none repeat scroll 0 0 #e6af4b;
+    }
+    .navsection ul li {
+        border-left: 1px solid #fdc662;
+        border-right: 1px solid #c18a26;
+    }
+    .samepost h2 a {
+        color: #ac7511;
+    }
+    .maincontent, .sidebar { background: #fef4e5 none repeat scroll 0 0; }
+    .samepost img {
+        border: 1px solid #ebb450;
+    }
+    .contentsection {
+        border: 1px solid #ca932f;
+    }
+    .samesidebar h2 {
+        background: #e6af4b none repeat scroll 0 0;
+        border-bottom: 2px solid #b7801c;
+        color: #333;
+    }
+    .readmore a {
+        border: 1px solid #b7801c;
+        color: #b7801c;
+    }
+    .readmore a:hover {
+        color: #E16525;
+    }
+    .pagination a {
+        background: #E6AF4B;
+        border: 1px solid #B7801C;
+    }
+    .pagination a:hover {
+        background: #BC8521;
+        color: #fff;
+    }
+    .footermenu ul li a {
+        color: #E6AF4B;
+    }
+    .samepost h2, .about h2 {
+        color: #ac7511;
+    }
+    .about img {
+        border: 1px solid #b7801c;
+    }
+    .relatedpost h2 {
+        background: #e6af4b none repeat scroll 0 0;
+        border-bottom: 2px solid #b7801c !important;
+    }
+    .samesidebar ul li a {
+        color: #814a00;
+    }
+    .samesidebar ul li a:hover {
+        color: #DF5C25;
+    }
+    .popular img {
+        border: 1px solid #f6bf5b;
+    }
+    /*Green*/
+    .headersection, .footersection { background: #008000 none repeat scroll 0 0; }
+    .icon a { background: #2DAD2D none repeat scroll 0 0; }
+    .searchbtn input[type="text"] { background: #46C646; border: 1px solid #196B19; }
+    .searchbtn input[type="submit"] {
+        border: 1px solid #267326;
+    }
+    input[type="submit"] {
+        background: #064906 none repeat scroll 0 0;
+        border: 1px solid #267326;
+    }
+    .searchbtn input[type="submit"]:hover {
+        background: #46C646 none repeat scroll 0 0;
+        border: 1px solid #1B6A1B;
+    }
+    .navsection {
+        background: none repeat scroll 0 0 #49C949;
+    }
+    .navsection ul li {
+        border-left: 1px solid #2B492B;
+        border-right: 1px solid #24A424;
+    }
+    .samepost h2 a {
+        color: #11ac2e;
+    }
+    .maincontent, .sidebar { background: #A0FFA0 none repeat scroll 0 0; }
+    .samepost img {
+        border: 1px solid #49C949;
+    }
+    .contentsection {
+        border: 1px solid #49C949;
+    }
+    .samesidebar h2 {
+        background: #49C949 none repeat scroll 0 0;
+        border-bottom: 2px solid #008000;
+        color: #fff;
+    }
+    .readmore a {
+        border: 1px solid #49C949;
+        color: #49C949;
+    }
+    .readmore a:hover {
+        color: #4C710F;
+    }
+    .pagination a {
+        background: #49C949;
+        border: 1px solid #008000;
+    }
+    .pagination a:hover {
+        background: #008000;
+        border: 1px solid #49C949;
+        color: #fff;
+    }
+    .footermenu ul li a {
+        color: #49C949;
+    }
+    .samepost h2, .about h2 {
+        color: #008000;
+    }
+    .about img {
+        border: 1px solid #008000;
+    }
+    .relatedpost h2 {
+        background: #49C949 none repeat scroll 0 0;
+        border-bottom: 2px solid #008000 !important;
+    }
+    .samesidebar ul li a {
+        color: #008000;
+    }
+    .samesidebar ul li a:hover {
+        color: #73CC3D;
+    }
+    .popular img {
+        border: 1px solid #49C949;
+    }
+
+    /*Red*/
+
+</style>
