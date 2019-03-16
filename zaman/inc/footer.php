7638906a7a46ee6086a499c40a69c1d5dd75aaa0
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2019-03-16 11:14:40 +0600

    Update footer.php file on zaman/inc folder

--- zaman/inc/footer.php
+++ zaman/inc/footer.php
@@ -6,7 +6,7 @@
     </div>
     <div id="site_info">
         <p>
-         &copy; Copyright <a href="http://zamanwebdeveloper.com">Zaman Web Development Ltd.</a>. All Rights Reserved.
+         &copy; Copyright <a href="https://www.zamanwebdeveloper.com">Zaman Web Development Ltd.</a>. All Rights Reserved.
         </p>
     </div>
 </body>
